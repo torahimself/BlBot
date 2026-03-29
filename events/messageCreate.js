b3e4271394db19a34cafd3fdea9533807bd9fa80
torahimself <torahimself@gmail.com>
2026-03-29 18:49:50 +0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,11 @@
+const { processMessage } = require('../utils/economy/wordTracker.js');
 
+module.exports = {
+  name: 'messageCreate',
+  async execute(message) {
+    // Ignore bot messages and commands
+    if (message.author.bot) return;
+    if (message.content.startsWith('/')) return;
+    await processMessage(message);
+  },
+};
