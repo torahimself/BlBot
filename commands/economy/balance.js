4dc2a5331a438c232ddb433e9dee7e3ad6151dc5
torahimself <torahimself@gmail.com>
2026-03-29 18:41:47 +0800

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { getBalance } = require('../../utils/economy/shopManager.js');
+const allowedChannels = ['1415933682748751923', '1464140979148689550'];
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('balance')
+    .setDescription('Check your coin balance'),
+  async execute(interaction) {
+    if (!allowedChannels.includes(interaction.channelId)) {
+      return interaction.editReply(`❌ This command can only be used in <#1415933682748751923> or <#1464140979148689550>.`);
+    }
+    const balance = await getBalance(interaction.user.id);
+    await interaction.editReply(`💰 Your balance: **${balance}** coins.`);
+  }
+};
