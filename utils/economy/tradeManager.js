e51e13e955361a7ccd9c68938ce637983955edd7
torahimself <torahimself@gmail.com>
2026-04-12 06:16:14 +0800

    Update tradeManager.js

--- utils/economy/tradeManager.js
+++ utils/economy/tradeManager.js
@@ -1,58 +1,54 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-
 const activeTrades = new Map();
 
 class Trade {
-    constructor(initiatorId, targetId, initiatorOffer) {
-        this.id = `${Date.now()}_${Math.random()}`;
-        this.initiatorId = initiatorId;
-        this.targetId = targetId;
-        this.initiatorOffer = initiatorOffer;
-        this.targetOffer = null;
-        this.initiatorConfirmed = false;
-        this.targetConfirmed = false;
-        this.createdAt = Date.now();
-        // Increase timeout to 2 minutes (120 seconds)
-        this.timeout = setTimeout(() => this.cancel('Trade timed out after 2 minutes.'), 120000);
-    }
+  constructor(initiatorId, targetId, initiatorOffer) {
+    // Use a clean numeric-only ID so it won't break when embedded in button customIds split by '_'
+    this.id = `${Date.now()}${Math.floor(Math.random() * 100000)}`;
+    this.initiatorId = initiatorId;
+    this.targetId = targetId;
+    this.initiatorOffer = initiatorOffer;
+    this.targetOffer = null;
+    this.initiatorConfirmed = false;
+    this.targetConfirmed = false;
+    this.createdAt = Date.now();
+    this.timeout = setTimeout(() => this.cancel('Trade timed out after 2 minutes.'), 120000);
+  }
 
-    cancel(reason) {
-        clearTimeout(this.timeout);
-        activeTrades.delete(this.id);
-        // Notify users via DM (optional)
-        // We'll handle it in the command
-    }
+  cancel(reason) {
+    clearTimeout(this.timeout);
+    activeTrades.delete(this.id);
+  }
 
-    setTargetOffer(offer) {
-        this.targetOffer = offer;
-    }
+  setTargetOffer(offer) {
+    this.targetOffer = offer;
+  }
 
-    confirm(userId) {
-        if (userId === this.initiatorId) this.initiatorConfirmed = true;
-        else if (userId === this.targetId) this.targetConfirmed = true;
+  confirm(userId) {
+    if (userId === this.initiatorId) this.initiatorConfirmed = true;
+    else if (userId === this.targetId) this.targetConfirmed = true;
 
-        if (this.initiatorConfirmed && this.targetConfirmed) {
-            clearTimeout(this.timeout);
-            activeTrades.delete(this.id);
-            return true;
-        }
-        return false;
+    if (this.initiatorConfirmed && this.targetConfirmed) {
+      clearTimeout(this.timeout);
+      activeTrades.delete(this.id);
+      return true;
     }
+    return false;
+  }
 }
 
 function createTrade(initiatorId, targetId, initiatorOffer) {
-    const trade = new Trade(initiatorId, targetId, initiatorOffer);
-    activeTrades.set(trade.id, trade);
-    return trade;
+  const trade = new Trade(initiatorId, targetId, initiatorOffer);
+  activeTrades.set(trade.id, trade);
+  return trade;
 }
 
 function getTrade(tradeId) {
-    return activeTrades.get(tradeId);
+  return activeTrades.get(tradeId);
 }
 
 function cancelTrade(tradeId, reason) {
-    const trade = activeTrades.get(tradeId);
-    if (trade) trade.cancel(reason);
+  const trade = activeTrades.get(tradeId);
+  if (trade) trade.cancel(reason);
 }
 
 module.exports = { activeTrades, createTrade, getTrade, cancelTrade };
