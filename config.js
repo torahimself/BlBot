5b69853133cdcbf17b3f7293b8cb1344aa641d4a
torahimself <torahimself@gmail.com>
2026-07-19 10:17:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,24 +12,6 @@ module.exports = {
       "1357384022388379891"
     ],
     rotationInterval: 24 * 60 * 60 * 1000,
-    // Extra per-role permission overwrites applied to every newly-created
-    // rotation channel, on top of whatever is cloned from the template
-    // channel. If a role already has an overwrite from the template, these
-    // permissions are merged into it (added, not replacing existing allows).
-    additionalPermissionOverwrites: [
-      {
-        roleId: "1408975467540779088",
-        allow: [
-          "ViewChannel",
-          "SendMessages",
-          "ReadMessageHistory",
-          "EmbedLinks",
-          "AttachFiles",
-          "UseExternalEmojis",
-          "ManageMessages",
-        ],
-      },
-    ],
   },
 
   // Attachment counter system config
